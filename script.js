99d57b46cfd3db65abac383d12ffd57c4b55b468
Alexander2119m <105405177+Alexander2119m@users.noreply.github.com>
2023-04-17 21:03:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// obtiene el formulario
+const form = document.querySelector('#registro');
+
+// maneja el envío del formulario
+form.addEventListener('submit', (event) => {
+	event.preventDefault(); // previene el comportamiento predeterminado de enviar el formulario
+
+	// obtiene los valores de los campos del formulario
+	const nombre = document.querySelector('#nombre').value;
+	const apellido = document.querySelector('#apellido').value;
+	const email = document.querySelector('#email').value;
+	const telefono = document.querySelector('#telefono').value;
+
+	// realiza la validación de los campos requeridos
+	if (nombre === '' || apellido === '' || email === '' || telefono === '') {
+		alert('Por favor, complete todos los campos.');
+		return;
+	}
+
+	// crea un objeto con los datos del cliente
+	const cliente = {
+		nombre,
+		apellido,
+		email,
+		telefono
+	};
+
+	// guarda los datos del cliente en una base de datos o en el almacenamiento local
+	// aquí se asume que se están almacenando en el almacenamiento local del navegador
+	localStorage.setItem('cliente', JSON.stringify(cliente));
+
+	// muestra un mensaje de confirmación
+	alert('Los datos del cliente han sido guardados.');
+
+	// reinicia el formulario
+	form.reset();
+	
+});
